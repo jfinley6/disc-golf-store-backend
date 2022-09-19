2cdf317abbc15d215fce64263198201d86028bc9
johnfinley <johnfinleybremerton@gmail.com>
2022-09-18 19:11:18 -0600

    init

--- db/schema.rb
+++ db/schema.rb
@@ -14,23 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_182132) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.string "body"
-    t.integer "user_id"
-    t.integer "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "subject"
-    t.text "body"
-    t.string "image_url"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
