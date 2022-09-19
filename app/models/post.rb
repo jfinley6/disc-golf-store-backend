eafa9ecd3978ba01307c09bbd888d3676a7fa629
johnfinley <johnfinleybremerton@gmail.com>
2022-09-19 17:19:52 -0600

    .

--- app/models/post.rb
+++
@@ -1,8 +0,0 @@
-class Post < ApplicationRecord
-    belongs_to :user
-    has_many :comments, dependent: :destroy
-
-    validates :subject, presence: true
-    validates :body, presence: true
-    validates :user_id, presence: true
-end
